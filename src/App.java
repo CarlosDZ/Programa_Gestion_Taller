e8e9f8e0345879ae9b2c77257ad03f3d9a1384c3
Carlos Delgado <carlos.dz16180@gmail.com>
2025-03-26 09:53:18 +0100

    Feat: Menu de opciones de cliente

--- src/App.java
+++ src/App.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 
 
 public class App {
-    
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Bienvenido al programa de gestion de " + Taller.getName());
 
@@ -15,9 +15,7 @@ public class App {
             main_menu_option = scanner.nextInt();
 
             switch (main_menu_option) {
-                case 1 -> {
-                    //menuclientes
-                }
+                case 1 -> { menuClientes(); }
                 case 2 -> {
                     //menuvehiculos
                 }
@@ -37,11 +35,47 @@ public class App {
                     //menuservicios
                 }
                 case 8 -> { System.out.println("Saliendo del programa!"); }
-                
+
                 default -> System.out.println("El numero no esta en el rango especificado, introduce un numero entre 1 y 8.");
             }
         } while (main_menu_option != 8);
 
+    }
+
+    public static void menuClientes(){
+        int menu_clientes_opcion;
+
+        do { 
+            System.out.println("Que quieres hacer?\n1 - Ver todos los clientes\n2 - Nuevo cliente\n3 - Borrar cliente\n 4 - Editar cliente\n5 - Vincular coche a cliente\n6 - Ver informacion detallada de un cliente\n7 - Salir del menu de clientes");
+            menu_clientes_opcion = scanner.nextInt();
+
+            switch (menu_clientes_opcion) {
+                case 1 -> {
+                    //Taller.printAllCustomers()
+                }
+                case 2 -> {
+                    //Taller.newCustomer()
+                }
+                case 3 -> {
+                    //Taller.delCustomer()
+                }
+                case 4 -> {
+                    //Cliente.edit()
+                }
+                case 5 -> {
+                    //Car.linkToCliente()
+                }
+                case 6 -> {
+                    //Cliente.details()
+                }
+                case 7 -> {
+                    System.out.println("Saliendo del menu de clientes...");
+                }
+
+                default -> System.out.println("El numero no esta en el rango especificado, introduce un numero entre 1 y 7.");
+            }
+
+        } while (menu_clientes_opcion != 7);
     }
 
 }
--- src/Taller.java
+++ src/Taller.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 public class Taller {
     private static final String NAME = "Nombre_Taller";
-
+    static Scanner scanner = new Scanner(System.in);
 
 
     public static String getName(){ return Taller.NAME; }
+
     
 }
