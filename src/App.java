1635a85842e1e228d084294500f0fbb9e5a0f1e6
Carlos Delgado <carlos.dz16180@gmail.com>
2025-03-26 10:48:48 +0100

    Feat: Menu citas y servicios

--- src/App.java
+++ src/App.java
@@ -172,4 +172,64 @@ public class App {
         } while (menu_fabricantes_opcion != 4);
     }
 
+    public static void menuCitas(){
+        int menu_citas_opcion;
+
+        do { 
+            System.out.println("Que quieres hacer?\n1 - Ver todas las citas\n2 - Nueva cita\n3 - Borrar cita\n4 - Editar cita\n5 - Gestionar servicios\n6 - Salir del menu de citas");
+            menu_citas_opcion = scanner.nextInt();
+
+            switch (menu_citas_opcion) {
+                case 1 -> {
+                    //Taller.printAllCitas()
+                }
+                case 2 -> {
+                    //Taller.newCita()
+                }
+                case 3 -> {
+                    //Taller.delCita()
+                }
+                case 4 -> {
+                    //Cita.edit()
+                }
+                case 5 -> { menuServicios(); }
+                case 6 -> { System.out.println("Saliendo del menu de citas..."); }
+                default -> System.out.println("El numero no esta en el rango especificado, introduce un numero entre 1 y 6.");
+            }
+
+        } while (menu_citas_opcion != 6);
+    }
+
+    public static void menuServicios(){
+        int menu_servicios_opcion;
+
+        do { 
+            System.out.println("Que quieres hacer?\n1 - Ver todos los servicios\n2 - Nuevo servicio\n3 - Borrar servicio\n4 - Editar servicio\n5 - Informacion detallada de un servicio\n6 - Salir del menu de servicios");
+            menu_servicios_opcion = scanner.nextInt();
+
+            switch (menu_servicios_opcion) {
+                case 1 -> {
+                    //Taller.printAllServicios()
+                }
+                case 2 -> {
+                    //Taller.newServicio()
+                }
+                case 3 -> {
+                    //Taller.delServicio()
+                }
+                case 4 -> {
+                    //Servicio.edit()
+                }
+                case 5 -> {
+                    //Servicio.details()
+                }
+                case 6 -> {
+                    System.out.println("Saliendo del menu de servicios...");
+                }
+
+                default -> System.out.println("El numero no esta en el rango especificado, introduce un numero entre 1 y 6.");
+            }
+
+        } while (menu_servicios_opcion != 6);
+    }
 }
