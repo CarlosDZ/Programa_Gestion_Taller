54255fcbc946eadc06dfbc7e56e8637b60755b7c
Carlos Delgado <carlos.dz16180@gmail.com>
2025-04-04 13:18:48 +0200

    Fix: Fix al mostrar el historial teniendo transacciones sin concepto

--- src/view/TransaccionView.java
+++ src/view/TransaccionView.java
@@ -4,8 +4,16 @@ import model.Transaccion;
 
 public class TransaccionView {
     public static void viewHistorial(ArrayList<Transaccion> historial){
+        int longitudConcepto;
         for(Transaccion transaccion : historial){
-            System.out.println("("+transaccion.getId()+")   "+transaccion.getConcepto().substring(0, 25)+"   "+transaccion.getCantidad()+"€   "+transaccion.getFecha());
+             longitudConcepto = transaccion.getConcepto().length();
+             String newConcepto;
+            if(longitudConcepto > 25)
+                newConcepto = transaccion.getConcepto().substring(0, 25);
+            else
+                newConcepto = transaccion.getConcepto();
+
+            System.out.println("("+transaccion.getId()+")   "+newConcepto+"   "+transaccion.getCantidad()+"€   "+transaccion.getFecha());
         }
     }
 }
